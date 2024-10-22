df80381ee38ec0f08a4ffd40671606a2b2d1dea1
Anderson22-dev <andersonsilvasouza714@gmail.com>
2024-10-22 10:13:03 -0300

    fix: localstorage is not defined

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import OrdersTable from "@/components/pages/dashboard/OrdersTable";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+  useEffect(() => {
+    if (!window.localStorage.getItem("token")) {
+      return redirect("/signin");
+    }
+  }, []);
+
   return (
     <div className="px-6 py-10 flex flex-col gap-[6.25rem]">
       <h1 className="font-medium text-5xl">Visualizar Pedidos</h1>
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,16 +1,11 @@
-"use client";
 import Sidebar from "@/components/sidebar/SideBar";
 import React from "react";
-import { redirect } from "next/navigation";
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  if (!localStorage.getItem("token")) {
-    return redirect("/signin");
-  }
   return (
     <div className="flex">
       <div className="w-min h-full">
